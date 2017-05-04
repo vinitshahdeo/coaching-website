b39f91b5b7c3a0ea2ec6dc0cdb5f4ffd53c275d2
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-05-05 01:46:58 +0530

    javascript

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+/*
+	JavaScript For Responsive Bootstrap Carousel
+*/
+
+$(function ($) {
+    "use strict";
+    function doAnimations(elems) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+        elems.each(function () {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+    //Variables on page load
+    var $immortalCarousel = $('.animate_text'),
+        $firstAnimatingElems = $immortalCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+    //Initialize carousel
+    $immortalCarousel.carousel();
+    //Animate captions in first slide on page load
+    doAnimations($firstAnimatingElems);
+    //Other slides to be animated on carousel slide event
+    $immortalCarousel.on('slide.bs.carousel', function (e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
+    });
+
+})(jQuery);
